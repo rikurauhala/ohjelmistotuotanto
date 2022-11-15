e3a0e9c518a03acef3f109568b5b45929cce60ef
Riku Rauhala <riku@rauhala.io>
2022-11-15 09:35:22 +0200

    Print Finnish NHL players

--- palautukset/viikko3/nhl-reader/src/index.py
+++ palautukset/viikko3/nhl-reader/src/index.py
@@ -1,7 +1,28 @@
+import requests
+
 from player import Player
 
+
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
+    response = requests.get(url).json()
+
+    players = []
+    for player_dict in response:
+        player = Player(
+            player_dict["name"],
+            player_dict["team"],
+            player_dict["nationality"],
+            player_dict["goals"],
+            player_dict["assists"]
+        )
+        players.append(player)
+
+    print("NHL players from Finland:")
+    for player in players:
+        if player.nationality == "FIN":
+            print(player)
+
 
 if __name__ == "__main__":
     main()
