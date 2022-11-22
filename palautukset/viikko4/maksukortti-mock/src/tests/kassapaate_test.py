985f00fe17fe57492ba73eedd04b014c4c1e33df
Riku Rauhala <riku@rauhala.io>
2022-11-22 19:32:42 +0200

    Fix kassapaate tests

--- palautukset/viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ palautukset/viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,19 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_lataaminen_onnistuu_positiivisella_summalla(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 0
+
+        self.kassa.lataa(maksukortti_mock, 10)
+
+        maksukortti_mock.lataa.assert_called_with(10)
+
+    def test_lataaminen_epaonnistuu_negatiivisella_summalla(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 0
+
+        self.kassa.lataa(maksukortti_mock, -10)
+
+        maksukortti_mock.lataa.assert_not_called()
