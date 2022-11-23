0656314136aa7feb94b90fa11d4a0b27e18578a4
Riku Rauhala <riku@rauhala.io>
2022-11-23 13:55:37 +0200

    Test and implement the price of items in a basket with two same products (step 7)

--- palautukset/viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ palautukset/viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -36,3 +36,8 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(self.maito)
         self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_oikea(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
+        self.assertEqual(self.kori.hinta(), 2 * self.maito.hinta())
