b410a96c9c5a403b169f1e634d2afffa382b7f5f
Riku Rauhala <riku@rauhala.io>
2022-11-23 13:19:35 +0200

    Test and implement the amount of purchases in a basket with two different items (step 4)

--- palautukset/viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ palautukset/viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -7,18 +7,22 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
+        self.maito = Tuote("Maito", 3)
+        self.piima = Tuote("Piimä", 2)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_tavara(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_sama_kuin_tuotteen_hinta(self):
-        hinta = 3
-        maito = Tuote("Maito", hinta)
-        self.kori.lisaa_tuote(maito)
-        self.assertEqual(self.kori.hinta(), hinta)
+        self.kori.lisaa_tuote(self.maito)
+        self.assertEqual(self.kori.hinta(), self.maito.hinta())
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorissa_kaksi_tavaraa(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.piima)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
