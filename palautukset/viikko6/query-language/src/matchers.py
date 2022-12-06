d2211a03e0f78dfb486f872ecc6e48b2e34f9fc0
Riku Rauhala <riku@rauhala.io>
2022-12-06 19:27:10 +0200

    Implement the Or matcher

--- palautukset/viikko6/query-language/src/matchers.py
+++ palautukset/viikko6/query-language/src/matchers.py
@@ -14,7 +14,6 @@ class And:
         for matcher in self._matchers:
             if not matcher.test(player):
                 return False
-
         return True
 
 
@@ -26,7 +25,6 @@ class Not:
         for matcher in self._matchers:
             if not matcher.test(player):
                 return True
-
         return False
 
 
@@ -37,7 +35,6 @@ class HasAtLeast:
 
     def test(self, player):
         player_value = getattr(player, self._attr)
-
         return player_value >= self._value
 
 
@@ -48,8 +45,18 @@ class HasFewerThan:
 
     def test(self, player):
         player_value = getattr(player, self._attr)
-
         return player_value < self._value
+
+
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+        return False
 
 
 class PlaysIn:
